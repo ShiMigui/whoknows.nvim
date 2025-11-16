afe3395935d9d7e3ddea003b123eaa3e3a0952dd
Miguel Nascimento <miguel.nasto@gmail.com>
2025-11-16 13:41:39 -0300

    Add(init.lua, vim/*): Basic vim UIs/Search/Indentation/Globals configs
    This commit adds vim configs to opts and globals, in `lua/vim/*.lua`
    files.

---
+++ lua/vim/init.lua
@@ -0,0 +1,5 @@
+require('vim.globals')
+require('vim.opts')
+
+local set = vim.keymap.set
+set('i', 'jk', '<ESC>')
---
+++ lua/vim/opts.lua
@@ -0,0 +1,26 @@
+vim.o.termguicolors = true
+vim.o.cursorline = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.swapfile = false
+vim.o.clipboard = "" -- If you want to use the same clipboard as your system, set this to 'unnamedplus', I don't like this btw
+vim.o.cmdheight = 0 -- Command-line will appear always typing a command, I like this config.
+
+-- Indentation
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.smartindent = true
+
+-- Search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.hlsearch = false
+
+-- UI
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.scrolloff = 5
+vim.o.signcolumn = "yes"
