16b3f6c43540f3f4d39eb7c519047b7dd6f477c1
Miguel Nascimento <miguel.nasto@gmail.com>
2025-11-24 16:15:25 -0300

    add: keymap to quit all others buffers

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -5,6 +5,15 @@ require("core.lazy")
 
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exits INSERT mode" })
 vim.keymap.set("n", "<leader>q", "<CMD>bd!<CR>", { desc = "Closes buffer" })
+vim.keymap.set("n", "<leader>Q", function()
+  local current = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) and buf ~= current then
+      vim.api.nvim_buf_delete(buf, { force = true })
+    end
+  end
+end, { desc = "Closes all others buffers" })
+
 
 if vim.loop.os_getenv("XDG_SESSION_TYPE") ~= "wayland" or vim.loop.os_getenv("WAYLAND_DISPLAY") == nil then return end
 vim.g.clipboard = {
