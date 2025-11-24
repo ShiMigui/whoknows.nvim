f439720298248a3f2d7191e040056df9d223d5ad
Miguel Nascimento <miguel.nasto@gmail.com>
2025-11-24 14:12:42 -0300

    refactor(core/): remove unnecessary globals.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,6 +1,15 @@
-require('core.globals')
+vim.g.mapleader = "\\" -- This is the key <LEADER>, change it to " " if you prefer using space
+
 require('core.options')
 require('core.lazy')
 require("core.tools.terminal")
 
 vim.keymap.set('i', 'jk', '<ESC>')
+
+if vim.loop.os_getenv("XDG_SESSION_TYPE") ~= "wayland" or vim.loop.os_getenv("WAYLAND_DISPLAY") == nil then return end
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = { ["+"] = "wl-copy", ["*"] = "wl-copy" },
+  paste = { ["+"] = "wl-paste", ["*"] = "wl-paste" },
+  cache_enabled = 1,
+}
