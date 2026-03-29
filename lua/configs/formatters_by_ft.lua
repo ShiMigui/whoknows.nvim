eb9c1af322139eb8b533e9093c10d00d5ba106ea
Miguel Nascimento <miguel.nasto@gmail.com>
2026-03-28 22:11:25 -0300

    temporary backup

--- lua/configs/formatters_by_ft.lua
+++
@@ -1,8 +0,0 @@
-return {
-	lua = { "stylua" },
-	js = { "prettier" },
-	jsx = { "prettier" },
-	tsx = { "prettier" },
-	css = { "prettier" },
-	scss = { "prettier" },
-}
--- lua/plugins/mason-catalog.lua
+++ lua/plugins/mason-catalog.lua
@@ -1,58 +1,46 @@
-local ts = "typescript-language-server"
 local mapping = require("core.actions")
-local list = {}
+local ok, conform = pcall(require, "conform")
+mapping.format = ok and conform.format or mapping.format
+
+local prettier_fts = { "js", "jsx", "tsx", "css", "scss" }
+local formatters_by_ft = {
+	lua = { "stylua" },
+	php = { "php_cs_fixer" },
+}
+for _, ft in ipairs(prettier_fts) do
+	formatters_by_ft[ft] = { "prettier" }
+end
 
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = { formatters_by_ft = require("configs.formatters_by_ft") },
-		config = function(_, opts)
-			local cfm = require("conform")
-			cfm.setup(opts)
-			mapping.format = cfm.format
-			for _, f in ipairs(cfm.list_all_formatters()) do
-				list[#list + 1] = f.name
-			end
-		end,
+	"ShiMigui/mason-catalog.nvim",
+	dependencies = {
+		{ "williamboman/mason.nvim", opts = {} },
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = { library = { { path = "${3rd}/luv/library", words = { "vim%.uv" } } } },
+		},
+		{ "stevearc/conform.nvim", opts = { formatters_by_ft = formatters_by_ft } },
+		{ "neovim/nvim-lspconfig", event = { "BufReadPre", "BufNewFile" } },
 	},
-	{
-		"ShiMigui/mason-catalog.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			{
-				"folke/lazydev.nvim",
-				ft = "lua",
-				opts = { library = { { path = "${3rd}/luv/library", words = { "vim%.uv" } } } },
-			},
-			{ "neovim/nvim-lspconfig", event = { "BufReadPre", "BufNewFile" } },
-		},
-		opts = {
-			lsp = {
-				default_config = { capabilities = require("cmp_nvim_lsp").default_capabilities() },
-				by_ft = {
-					lua = "lua-language-server",
-					javascriptsx = {ts},
-					tsx = ts,
-					javascript = ts,
-					typescript = ts,
+	opts = {
+		lsp = {
+			default_config = { capabilities = require("cmp_nvim_lsp").default_capabilities(), },
+			by_group = {
+				{
+					extensions = { "js", "ts", "jsx", "tsx" },
+					lsps = { "typescript-language-server", "eslint-lsp" },
 				},
-				auto_enable = true,
+				{ filetypes = { "json", "jsonc" }, lsps = { "json-lsp" } },
+				{ extensions = { "md" }, lsps = { "marksman" } },
 			},
-			ensure_installed = list,
+			by_ft = {
+				lua = "lua-language-server",
+				php = "intelephense",
+			},
 		},
-		config = function(_, opts)
-			require("mason").setup()
-			local registry = require("mason-registry")
-
-			local function setup()
-				require("mason-catalog").setup(opts)
-			end
-
-			if #registry.get_all_packages() == 0 then
-				registry.refresh(setup)
-			else
-				setup()
-			end
-		end,
+		integrations = { "ensure_conform_formatters" },
+		silent = true,
+		debug = true,
 	},
 }
