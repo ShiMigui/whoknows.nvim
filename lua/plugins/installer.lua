f4c657968fe84fcbef6abe1dacceb3df0125a8e7
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-21 20:52:13 -0300

    refactor: change mason-catalog opts to last update db6e183

--- lua/plugins/installer.lua
+++ lua/plugins/installer.lua
@@ -6,37 +6,38 @@ return {
 		{ "williamboman/mason.nvim", config = true },
 	},
 	opts = {
-		silent = true,
 		integrations = { "ensure_conform_formatters" },
 		ensure_installed = { "java-debug-adapter", "java-test", "pgformatter" },
 		lsp = {
 			default_config = require("settings").lsp,
-			by_group = {
-				{ extensions = { "js", "ts", "jsx", "tsx" }, lsps = { "typescript-language-server", "eslint-lsp" } },
-				{ extensions = { "json", "jsonc" }, lsps = { "json-lsp" } },
-				{ extensions = { "md" }, lsps = { "marksman" } },
+			extensions = {
+				{ "js", "ts", "jsx", "tsx", lsp = { "typescript-language-server", "eslint-lsp" } },
+				{ "json", "jsonc", lsp = { "json-lsp" } },
+				{ "md", lsp = { "marksman" } },
 			},
-			by_ft = {
-				sql = "postgres-language-server",
-				lua = "lua-language-server",
-				dockerfile = "dockerfile-language-server",
-				php = { "intelephense", "phpactor" },
-				java = { jdtls = { cmd = { "jdtls", "-data", vim.fn.stdpath("cache") .. "/jdtls-workspace" } } },
-				yaml = {
-					["yaml-language-server"] = {
-						settings = {
-							yaml = {
-								schemas = {
-									["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
-										"docker-compose.yml",
-										"docker-compose.yaml",
-										"compose.yml",
-										"compose.yaml",
+			filetypes = {
+				{ "sql", lsp = "postgres-language-server" },
+				{ "lua", lsp = { "lua-language-server" } },
+				{ "dockerfile", lsp = "dockerfile-language-server" },
+				{ "php", lsp = { "intelephense", "phpactor" } },
+				{
+					"yaml",
+					lsps = {
+						["yaml-language-server"] = {
+							settings = {
+								yaml = {
+									schemas = {
+										["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
+											"docker-compose.yml",
+											"docker-compose.yaml",
+											"compose.yml",
+											"compose.yaml",
+										},
 									},
+									validate = true,
+									completion = true,
+									hover = true,
 								},
-								validate = true,
-								completion = true,
-								hover = true,
 							},
 						},
 					},
