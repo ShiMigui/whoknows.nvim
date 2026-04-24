1c82cb5f33fa05ddfc68b4350ce22e57e77965ff
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-24 20:52:47 -0300

    refactor: update mason-catalog to new plugin catalog

--- lua/plugins/installer.lua
+++ lua/plugins/installer.lua
@@ -1,48 +1,26 @@
 return {
-	"ShiMigui/mason-catalog.nvim",
+	"ShiMigui/catalog.nvim",
 	dependencies = {
 		"neovim/nvim-lspconfig",
-		{ "mfussenegger/nvim-jdtls", ft = "java" },
-		{ "williamboman/mason.nvim", config = true },
+		{ "williamboman/mason.nvim", opts = {} },
 	},
 	opts = {
-		integrations = { "ensure_conform_formatters" },
-		ensure_installed = { "java-debug-adapter", "java-test", "pgformatter" },
+		conform = true,
 		lsp = {
-			default_config = require("settings").lsp,
-			extensions = {
-				{ "js", "ts", "jsx", "tsx", lsp = { "typescript-language-server", "eslint-lsp" } },
-				{ "json", "jsonc", lsp = { "json-lsp" } },
-				{ "md", lsp = { "marksman" } },
-			},
-			filetypes = {
-				{ "sql", lsp = "postgres-language-server" },
-				{ "lua", lsp = { "lua-language-server" } },
-				{ "dockerfile", lsp = "dockerfile-language-server" },
-				{ "php", lsp = { "intelephense", "phpactor" } },
-				{
-					"yaml",
-					lsps = {
-						["yaml-language-server"] = {
-							settings = {
-								yaml = {
-									schemas = {
-										["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
-											"docker-compose.yml",
-											"docker-compose.yaml",
-											"compose.yml",
-											"compose.yaml",
-										},
-									},
-									validate = true,
-									completion = true,
-									hover = true,
-								},
-							},
-						},
-					},
-				},
-			},
+			config = require("settings").lsp,
+
+			lua = "lua-language-server",
 		},
 	},
+	config = function(_, opts)
+		local registry = require("mason-registry")
+
+		if #registry.get_all_packages() > 0 then
+			require("catalog").setup(opts)
+		else
+			registry.refresh(function()
+				require("catalog").setup(opts)
+			end)
+		end
+	end,
 }
