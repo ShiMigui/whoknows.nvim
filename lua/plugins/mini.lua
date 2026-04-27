c3ec65272cdc7b970d608c549c70400c78acf4d4
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-27 20:45:12 -0300

    feat: add mini nvim-related module

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,22 @@
+local maps = require("keymaps")
+maps["open mini files"] = { lhs = "<C-e>", modes = { "n", "i" }, cmd = "lua MiniFiles.open()", esc = true }
+
+return {
+	"nvim-mini/mini.nvim",
+	version = false,
+	config = function()
+		require("mini.move").setup()
+		require("mini.pairs").setup()
+		require("mini.basics").setup()
+		require("mini.comment").setup()
+
+		require("mini.files").setup({
+			windows = { preview = true, width_preview = 40 },
+			content = {
+				filter = function(e)
+					return true
+				end,
+			},
+		})
+	end,
+}
